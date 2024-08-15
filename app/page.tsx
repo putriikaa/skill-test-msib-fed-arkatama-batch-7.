9e840f3e15af141413f7fb2fb58b750013ecafff
putriikaa <anandakusuma36@gmail.com>
2024-08-15 12:08:40 +0700

    update navbar

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const LandingPage = () => {
 
   return (
     <div className="p-10">
-      <header className="flex flex-row justify-center gap-10">
+      <header className="flex flex-row justify-center gap-10 mb-20">
         <Image src={logo} alt="" />
         <div className="flex flex-row gap-5 items-center">
           <p>About</p>
@@ -33,10 +33,42 @@ const LandingPage = () => {
           <p>Donasi</p>
           <p>Kontak</p>
           <p>Masuk</p>
-          <button className="color-red">Daftar Sekarang</button>
+          <button
+            className="bg-[#002EC1] text-white font-semibold py-2 px-4 rounded-full hover:bg-blue-700"
+            onClick={() => {}}
+          >
+            Daftar Sekarang
+          </button>
         </div>
       </header>
 
+      <div className="flex flex-row">
+        <div className="flex flex-col gap-[8px] w-auto">
+          <p className="text-[#002EC1]">
+            Mau Jadi Generasi Mahir Teknologi, Kuat Iman?
+          </p>
+          <p className="text-[#000] font-bold mb-[-20px] text-[48px] block">
+            Pondok Pesantren
+          </p>
+          <p className="text-[#002EC1] font-bold text-[48px]">
+            {"{Technopreneur}"}
+          </p>
+          <p>
+            Mencetak generasi unggul yang mahir di bidang teknologi digital dan
+            teguh dalam pemahaman Islam Ahlu Sunnah Wal Jama’ah
+          </p>
+          <div className="border-[#002EC1] border-2 py-2 px-4 rounded-full flex flex-row items-center gap-4">
+            <p>Jago IT Rajin Ngaji bersama Al Kautsar</p>
+            <button
+              className="bg-[#002EC1] text-white font-semibold py-2 px-4 rounded-full hover:bg-blue-700"
+              onClick={() => {}}
+            >
+              Download Profile Kami
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* 
       <section>
         <h2>Blogs</h2>
         {blogState?.status === "loading" && <p>Loading...</p>}
@@ -62,7 +94,7 @@ const LandingPage = () => {
           newsState?.data?.map((news: any) => (
             <div key={news?.id ?? ""}>{news?.judul ?? ""}</div>
           ))}
-      </section>
+      </section> */}
     </div>
   );
 };
